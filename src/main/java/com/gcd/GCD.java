06ece0669ffdc32d6fd5304fea005982f8197d7d
Michal Klempa <mkl@eea.sk>
2015-01-08 07:56:27 +0100

    Adding non-recursive version just for fun, no performance gain

--- src/main/java/com/gcd/GCD.java
+++ src/main/java/com/gcd/GCD.java
@@ -3,6 +3,31 @@ package com.gcd;
 public class GCD {
     public static int count;
 
+    public static int count2;
+
+    public static long computeGcd2(long a, long b) {
+        count2 = 0;
+        if (a < b) {
+            long t = a;
+            a = b;
+            b = t;
+        }
+        if (b == 0) {
+            return a;
+        }
+        while (a > 0) {
+            a = a % b;
+            count2++;
+            if (a == 0) {
+                break;
+            }
+            long t = a;
+            a = b;
+            b = t;
+        }
+        return b;
+    }
+
     public static long computeGcd(long a, long b) {
         count = 0;
         if (b > a) {
--- src/test/java/com/gcd/GCDTest.java
+++ src/test/java/com/gcd/GCDTest.java
@@ -2,6 +2,8 @@ package com.gcd;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.junit.Test;
@@ -101,5 +103,131 @@ public class GCDTest {
 
             }
         }
+    }
+
+    @Test
+    public void Btest() {
+        int a = 0;
+        int b = 0;
+        assertEquals(0, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest1() {
+        int a = 1;
+        int b = 0;
+        assertEquals(1, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest2() {
+        int a = 2;
+        int b = 0;
+        assertEquals(2, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest3() {
+        int a = 1;
+        int b = 1;
+        assertEquals(1, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest4() {
+        int a = 0;
+        int b = 10;
+        assertEquals(10, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest5() {
+        int a = 17;
+        int b = 25;
+        assertEquals(1, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest6() {
+        int a = 11;
+        int b = 13;
+        assertEquals(1, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest7() {
+        int a = 5;
+        int b = 5;
+        assertEquals(5, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest8() {
+        int a = 15;
+        int b = 5;
+        assertEquals(5, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest9() {
+        int a = 25;
+        int b = 15;
+        assertEquals(5, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest10() {
+        int a = 30;
+        int b = 20;
+        assertEquals(10, GCD.computeGcd2(a, b));
+    }
+
+    @Test
+    public void Btest11() {
+        int runs = 5000000;
+        Random random = new Random();
+        List<Long> values = new ArrayList<>(2 * runs);
+        for (int i = 0; i < 2 * runs; i++) {
+            values.add(Math.abs((long) random.nextInt()));
+        }
+
+        List<Long> gcds = new ArrayList<>(runs);
+        List<Long> counts = new ArrayList<>(runs);
+        List<Long> gcds2 = new ArrayList<>(runs);
+        List<Long> counts2 = new ArrayList<>(runs);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < runs; i++) {
+            long a = values.get(i);
+            long b = values.get(2 * i);
+            long gcd = GCD.computeGcd(a, b);
+            gcds.add(gcd);
+            counts.add((long) GCD.count);
+        }
+        System.out.println("GCD Time: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < runs; i++) {
+            long a = values.get(i);
+            long b = values.get(2 * i);
+            long gcd = GCD.computeGcd2(a, b);
+            gcds2.add(gcd);
+            counts2.add((long) GCD.count2);
+        }
+        System.out.println("GCD2 Time: " + (System.currentTimeMillis() - start));
+
+        for (int i = 0; i < runs; i++) {
+            long a = values.get(i);
+            long b = values.get(2 * i);
+            assertEquals(gcds.get(i), gcds2.get(i));
+            assertEquals(counts.get(i), counts2.get(i));
+
+            if (Math.log(a + b) / Math.log(2) < counts.get(i)) {
+                System.out.println("GCD(" + String.valueOf(a) + "," + String.valueOf(b) + ") = "
+                        + String.valueOf(gcds.get(i)) + ", count: " + String.valueOf(counts.get(i))
+                        + ", log_2(a+b): " + String.valueOf(Math.log(a + b) / Math.log(2)));
+            }
+        }
+
     }
 }
